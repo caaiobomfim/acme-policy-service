34a3d77bdb691d57c28cb22c91461fe6445ded06
Caio Bomfim <caaiobomfim@gmail.com>
2025-08-07 22:24:04 -0300

    :sparkles: feat: create interface for policy request handling methods

---
+++ src/main/java/com/acme/insurance/policy/domain/ports/PolicyService.java
@@ -0,0 +1,16 @@
+package com.acme.insurance.policy.domain.ports;
+
+import com.acme.insurance.policy.app.dto.PolicyRequestDto;
+import com.acme.insurance.policy.app.dto.PolicyResponseDto;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface PolicyService {
+
+    PolicyResponseDto createPolicy(PolicyRequestDto request);
+
+    PolicyResponseDto getPolicyById(UUID id);
+
+    List<PolicyResponseDto> getPoliciesByCustomerId(UUID customerId);
+}
