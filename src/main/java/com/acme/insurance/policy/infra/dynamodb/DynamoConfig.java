d95c444ffc6ceb5dc94ae92c167ae31c25786cb7
Caio Bomfim <caaiobomfim@gmail.com>
2025-08-08 23:01:05 -0300

    :feat: dynamodb: add configuration class with DynamoDbClient bean

---
+++ src/main/java/com/acme/insurance/policy/infra/dynamodb/DynamoConfig.java
@@ -0,0 +1,27 @@
+package com.acme.insurance.policy.infra.dynamodb;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+import java.net.URI;
+
+@Configuration
+public class DynamoConfig {
+    @Value("${aws.region}") private String region;
+    @Value("${aws.dynamodb.endpoint}") private String endpoint;
+
+    @Bean
+    public DynamoDbClient dynamoDbClient() {
+        return DynamoDbClient.builder()
+                .region(Region.of(region))
+                .endpointOverride(URI.create(endpoint))
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create("test", "test")))
+                .build();
+    }
+}
