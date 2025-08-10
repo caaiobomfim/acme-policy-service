e6544638517cb9553be235a09ee151fca8b0d1eb
Caio Bomfim <caaiobomfim@gmail.com>
2025-08-10 01:08:23 -0300

    :sparkles: feat: add SqsConfig class for SQS listener configuration

---
+++ src/main/java/com/acme/insurance/policy/infra/config/SqsConfig.java
@@ -0,0 +1,29 @@
+package com.acme.insurance.policy.infra.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsAsyncClient;
+
+import java.net.URI;
+
+@Configuration
+public class SqsConfig {
+
+    private static final Logger log = LoggerFactory.getLogger(SqsConfig.class);
+
+    @Bean
+    SqsAsyncClient sqsAsyncClient() {
+        return SqsAsyncClient.builder()
+                .endpointOverride(URI.create("http://localhost:4566"))
+                .region(Region.US_EAST_1)
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(AwsBasicCredentials.create("test", "test"))
+                )
+                .build();
+    }
+}
