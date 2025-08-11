4d3b441021a02e2f12c34a65d6b812c0a01b424a
Caio Bomfim <caaiobomfim@gmail.com>
2025-08-11 00:27:52 -0300

    :white_check_mark: test: add @ActiveProfiles("test") to PolicyServiceApplicationTests

--- src/test/java/com/acme/insurance/policy/PolicyServiceApplicationTests.java
+++ src/test/java/com/acme/insurance/policy/PolicyServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.acme.insurance.policy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PolicyServiceApplicationTests {
 
 	@Test
