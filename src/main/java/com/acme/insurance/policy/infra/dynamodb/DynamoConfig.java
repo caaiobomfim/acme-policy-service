996c8357671da8431d41566225618e0d2c5d1c52
Caio Bomfim <caaiobomfim@gmail.com>
2025-08-11 00:08:18 -0300

    :recycle: refactor(dynamo): use AppProps

--- src/main/java/com/acme/insurance/policy/infra/dynamodb/DynamoConfig.java
+++ src/main/java/com/acme/insurance/policy/infra/dynamodb/DynamoConfig.java
@@ -1,6 +1,7 @@
 package com.acme.insurance.policy.infra.dynamodb;
 
-import org.springframework.beans.factory.annotation.Value;
+import com.acme.insurance.policy.infra.config.AppProps;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
@@ -11,17 +12,20 @@ import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import java.net.URI;
 
 @Configuration
+@EnableConfigurationProperties(AppProps.class)
 public class DynamoConfig {
-    @Value("${aws.region}") private String region;
-    @Value("${aws.dynamodb.endpoint}") private String endpoint;
 
     @Bean
-    public DynamoDbClient dynamoDbClient() {
+    DynamoDbClient dynamoDbClient(AppProps props) {
+        var aws = props.aws();
         return DynamoDbClient.builder()
-                .region(Region.of(region))
-                .endpointOverride(URI.create(endpoint))
-                .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create("test", "test")))
+                .region(Region.of(aws.region()))
+                .endpointOverride(URI.create(aws.endpoints().dynamodb()))
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(
+                                AwsBasicCredentials.create(
+                                        aws.credentials().accessKey(),
+                                        aws.credentials().secretKey())))
                 .build();
     }
 }
